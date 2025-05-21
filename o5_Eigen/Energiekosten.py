dea834dcfb47c8c9bb82907c256c2186d8509e4d
m24158 <m24158@hb.dhbw-stuttgart.de>
2025-05-21 15:55:32 +0000

    21.05

---
+++ o5_Eigen/Energiekosten.py
@@ -0,0 +1,11 @@
+
+Hl = float(input ("Höhe der Heizleistung in kW: "))
+Hd = float(input("Länge der Heizdauer in h: "))
+Et = input("Wähle einen Energieträger (Gas/Öl/Strom): ")
+ 
+
+
+Erdgas = 0.09 * Hl 
+Erdöl = 0.11 * Hl
+Strom = 0.3 * Hl 
+
