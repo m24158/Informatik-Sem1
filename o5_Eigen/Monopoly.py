51577e4157a2b11c8c81377fccb5fa708982f291
m24158 <m24158@hb.dhbw-stuttgart.de>
2025-05-21 13:08:07 +0000

    21.05

--- o5_Eigen/Monopoly.py
+++ o5_Eigen/Monopoly.py
@@ -1,29 +1,35 @@
 import random
+AnzahlWurf = 0
 
-print("Sie sind im Gefängnis")
-string1 = input("Möchten sie sich freikaufen? (ja/nein):")
-
-if string1 == "ja" :
-    print("Sie kommen aus dem Gefängnis; -50$")
-
-elif string1 == "nein" :
-    i = 0
-    while i < 3 :
-        Wurf = random.randint(1,6)
-        Wurf2 = random.randint(1,6)
-        print( Wurf, Wurf2)
-        i = i+1 
-
-        if Wurf == Wurf2   :
-            print("Pasch - Sie kommen aus dem Gefängnis")
+if AnzahlWurf == 0 :
+    print("Sie sind im Gefängnis")
+    while True :
+        string1 = input("Möchten sie sich freikaufen? (ja/nein):")
+        if string1 == "ja" :
+            print("Sie kommen aus dem Gefängnis: -50$")
+            break 
+        elif string1 == "nein" :
+            while AnzahlWurf < 3 :
+                Wurf = random.randint(1,6)
+                Wurf2 = random.randint(1,6)
+                print( Wurf, Wurf2)
+                AnzahlWurf = AnzahlWurf + 1
+                if Wurf == Wurf2   :
+                    print("Pasch - Sie kommen aus dem Gefängnis")
+                    break
+                elif AnzahlWurf == 3 :
+                    print("Würfelversuche vorbei, Sie müssen 50$ zahlen")
             break
-    
-    
-        elif i == 3 :
-            print("Würfelversuche vorbei, Sie müssen 50$ zahlen")
 
-else : 
-    print("Bitte entscheidemn!")
+        elif string1 != "ja" or "nein" :
+            print("Bitte entscheidemn!")
+
+
+        
+        
+         
+
+
 
     
 
