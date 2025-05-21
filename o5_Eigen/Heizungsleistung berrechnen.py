e89361ed391661881d9a3d95e8e6ec52f45f0716
m24158 <m24158@hb.dhbw-stuttgart.de>
2025-05-21 14:58:32 +0000

    21.05

--- o5_Eigen/Heizungsleistung berrechnen.py
+++ o5_Eigen/Heizungsleistung berrechnen.py
@@ -1,16 +1,24 @@
 
-breite = float(input ("Raumbreite in m: "))
-tiefe = float(input ("Raumtiefe in m: "))
-höhe = float(input ("Raumhöhe in m: "))
-außen = float(input ("Temperatur außen in °C: "))
-innen = float(input ("Temperautr innen in °C: "))
+def eingabe_der_Werte(): #lässt sich einklappen, dadurch kürzer
+    breite = float(input ("Raumbreite in m: "))
+    tiefe = float(input ("Raumtiefe in m: "))
+    höhe = float(input ("Raumhöhe in m: "))
+    außen = float(input ("Temperatur außen in °C: "))
+    innen = float(input ("Temperautr innen in °C: "))
 
-Volumen = breite * tiefe * höhe 
-dT = innen - außen
-Heizleistung = Volumen * dT * 0.024
+    Volumen = breite * tiefe * höhe 
+    dT = innen - außen
+    return Volumen, dT #Werte zurückgeben
+Volumen, dT = eingabe_der_Werte() 
 
 if dT < 0 :
-    print("Achtung, die Temperatur liegt unter Null")
+    print("Achtung! Die Temperaturdifferenz ist kleiner als Null")
+
+def berechnung_Heizleistung ():
+   return Volumen * dT * 0.024
+
+Heizleistung = berechnung_Heizleistung()
+
 
 print (f"Die benötigte Heizleistung beträgt: {Heizleistung} kW")
 
